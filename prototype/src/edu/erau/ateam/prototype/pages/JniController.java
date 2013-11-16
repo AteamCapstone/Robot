7ce54d6af473d1c1421912e348f075f4d495efd9
KestlerC <kingc6@my.erau.edu>
2013-11-16 03:30:07 -0500

    Modified interface code to load .dll from local file system

--- prototype/src/edu/erau/ateam/prototype/pages/JniController.java
+++ prototype/src/edu/erau/ateam/prototype/pages/JniController.java
@@ -1,16 +1,20 @@
 package edu.erau.ateam.prototype.pages;
 
+import java.io.File;
+
 /** A class used to interface with C/C++ */
 final class JniController {
 	static final JniController instance = new JniController();
 	
 	private JniController(){}
+		
 	
 	static{
 		try {
-			System.loadLibrary("PrototypeNative");
-		} catch (Error e) {
+			System.load(new File("PrototypeNative.dll").getAbsolutePath());
+		} catch (Error e2) {
 			System.out.println("PrototypeNative.dll not found");
+			e2.printStackTrace();
 		}
 	}
 	
