28b0e756efbf5175061df6e0afb768a85aad4617
KestlerC <kingc6@my.erau.edu>
2013-11-16 08:16:19 -0500

    Added new functionality for the motor demo

--- prototype/src/edu/erau/ateam/prototype/pages/DemoMotorPage.java
+++ prototype/src/edu/erau/ateam/prototype/pages/DemoMotorPage.java
@@ -1,11 +1,55 @@
 package edu.erau.ateam.prototype.pages;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import edu.erau.ateam.prototype.LinkedPage;
+import static edu.erau.ateam.prototype.Setting.*;
+
 @SuppressWarnings("serial")
 /** The page used for demonstrating the motors. To be fleshed out later*/
-public class DemoMotorPage extends PlaceHolderPage{
-
+public class DemoMotorPage extends LinkedPage{
+	public DemoMotorPage(){
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		ButtonGroup group = new ButtonGroup();
+		
+		//creates a button and space for each state
+		for(MotorState state: MotorState.values()){
+			add(Box.createVerticalGlue());
+			
+			StateButton button = new StateButton(state);
+			add(button);
+			group.add(button);
+			button.setAlignmentX(0.5f);
+		}
+		add(Box.createVerticalGlue());
+	}
+	
 	@Override
 	protected String getPageName() {
 		return "Motor Demo";
+	}
+}
+
+/** Radio buttons that change the state of the motor */
+@SuppressWarnings("serial")
+class StateButton extends JRadioButton{
+	StateButton(final MotorState state){
+		super(state.toString());
+		setFont(LARGE_FONT.deriveFont(Font.BOLD,36));
+		setPreferredSize(BUTTON_SIZE);
+		setMaximumSize(BUTTON_SIZE);
+		
+		//changes the motor state
+		addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JniController.instance.setMotorState(state);
+			}
+		});
 	}
 }
--- prototype/src/edu/erau/ateam/prototype/pages/JniController.java
+++ prototype/src/edu/erau/ateam/prototype/pages/JniController.java
@@ -5,10 +5,34 @@ import java.io.File;
 /** A class used to interface with C/C++ */
 final class JniController {
 	static final JniController instance = new JniController();
+	/** The current state of the motors */
+	private MotorState currentState = MotorState.STOP;
 	
 	private JniController(){}
 		
+	/** Tells the c code to have the motor go backwards */
+	private native void motorBackwards();
 	
+	/** Tells the c code to have the motor go backwards */
+	private native void motorForward();
+	
+	/** Tells the c code to have the motor go backwards */
+	private native void stopMotor();
+
+	/** Changes the state of the motor */
+	void setMotorState(MotorState state){
+		if(currentState == state)return;//quit if no state change
+		currentState = state;
+		switch(state){
+			case FORWARD: motorForward();
+			break;
+			case BACKWARD: motorBackwards();
+			break;
+			default: stopMotor();
+		}
+	}
+	
+	/** static initializer loads the library */
 	static{
 		try {
 			System.load(new File("PrototypeNative.so").getAbsolutePath());
@@ -22,5 +46,12 @@ final class JniController {
 	 */
 	native int demoJni(int x, int y);
 	
+	/** Grabs the value in inches from the sonar */
 	native float readSonar();
+}
+
+enum MotorState{
+	FORWARD,
+	STOP,
+	BACKWARD;
 }
